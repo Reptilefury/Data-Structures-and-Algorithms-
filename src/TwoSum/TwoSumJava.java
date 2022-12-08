38d9d042a96042dea5c98c84122bbee4ff4c9b10
ryan <ryanwa@ueab.ac.ke>
2022-12-08 09:21:10 +0300

    Two sum Java

---
+++ src/TwoSum/TwoSumJava.java
@@ -0,0 +1,31 @@
+package TwoSum;
+import java.util.HashMap;
+class TwoSumJava{
+    //Function
+    public int[] twoSum(int[] nums,int target){
+        //hashmap variable
+        HashMap<Integer,Integer> prevMap = new  HashMap<>();
+        for (int i = 0; i < nums.length; i++ ){
+
+            int num =  nums[i];
+            int diff = target - num;
+
+            if (prevMap.containsKey(nums[i])){
+                return  new int[] {prevMap.get(num),i};
+            }
+
+            prevMap.put(diff,i);
+        }
+        return  new int[] {};
+    }
+}
+
+
+
+
+
+
+
+
+
+
