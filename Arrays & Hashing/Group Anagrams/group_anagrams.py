a22c79039f01ba39e795ce32631bb72418ac3c9f
ryan <ryanwa@ueab.ac.ke>
2023-01-19 09:58:58 +0300

    top freq

--- Arrays & Hashing/Group Anagrams/group_anagrams.py
+++ Arrays & Hashing/Group Anagrams/group_anagrams.py
@@ -1,7 +1,6 @@
 # Given an array of strings group the anagrams together
 import collections
 
-
 class Soln:
     def groupAnagram(self, strs: list[str]):
         hash = collections.defaultdict(list)
@@ -24,6 +23,22 @@ class Soln:
                 count[ord(char) - ord('a')] += 1
             hash_map[tuple(count)].append(string)
         return hash_map.values()
+
+
+
+
+#Given an array of strings group the anagrams and return the answer in any order
+class Soln2:
+    def group_anagram(self,list_string:list[str])->list[list[str]]:
+        hash_map = collections.defaultdict(list)
+        for string in list_string:
+            count = [0] * 26
+            for char in string:
+                count[ord(char) - ord('a')] += 1
+            hash_map[tuple(count)].append(string)
+        return  hash_map.values()
+
+
 
 
 
