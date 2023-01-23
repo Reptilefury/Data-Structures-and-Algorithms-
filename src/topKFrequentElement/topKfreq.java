59b218549cbb02ef668f33c54bee1f63f6fafc0d
ryan <ryanwa@ueab.ac.ke>
2023-01-23 09:36:35 +0300

    leet code

--- src/topKFrequentElement/topKfreq.java
+++ src/topKFrequentElement/topKfreq.java
@@ -1,4 +1,5 @@
 package topKFrequentElement;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
@@ -9,14 +10,105 @@ public class topKfreq {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : nums) map.put(num, map.getOrDefault(num, 0));
         PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
-        for (Map.Entry<Integer, Integer> it:map.entrySet()){
+        for (Map.Entry<Integer, Integer> it : map.entrySet()) {
             pq.add(it);
-            if(pq.size() > k){ pq.poll(); }
-            int i = k;
-            while (!pq.isEmpty()){
-                arr[--i]  = pq.poll().getKey();
+            if (pq.size() > k) {
+                pq.poll();
             }
+            int i = k;
+            while (!pq.isEmpty()) {
+                arr[--i] = pq.poll().getKey();
+            }
+        }
+        return arr;
+    }
+}
+
+
+class Solution1 {
+    public int[] topKfreq(int[] nums, int k) {
+        int[] arr = new int[k];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+        for (Map.Entry<Integer,Integer> it: map.entrySet()){
+            pq.add(it);
+            if (pq.size()>k){
+                pq.poll();
+            }
+        }
+        int i = k;
+        while (!pq.isEmpty()){
+            arr[--i] = pq.poll().getKey();
         }
         return  arr;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
