49b9eac5e64920ae901006cb8d66640ef72c0206
ryan <ryanwa@ueab.ac.ke>
2023-01-23 09:55:53 +0300

    top K freq

---
+++ src/topKFrequentElement/topKkotln.kt
@@ -0,0 +1,36 @@
+package topKFrequentElement
+class topKkotln{
+    fun topKkotln(nums:List<Int>,k:Int): IntArray {
+        val res = mutableListOf<Int>()
+        val count = HashMap<Int,Int>()
+        val freq = MutableList<MutableList<Int>>(nums.size +1){
+            mutableListOf()
+        }
+        for (n in nums){
+            count[n]=count.getOrDefault(n,0) + 1
+        }
+        for ((n,c) in count){
+            freq[c].add(n)
+        }
+        for(i in freq.size - 1 downTo 0){
+            for(n in freq[i]){
+                res.add(n)
+                if(res.size ==k){
+                    return res.toIntArray()
+                }
+            }
+        }
+        return  intArrayOf()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
