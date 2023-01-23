35819644518432a4d2382f7786732a16d642eaea
ryan <ryanwa@ueab.ac.ke>
2023-01-23 09:37:12 +0300

    data structures and algo

--- Arrays & Hashing/Top K frequent Elements/top_k_freq.py
+++ Arrays & Hashing/Top K frequent Elements/top_k_freq.py
@@ -1,35 +1,55 @@
 class Solution:
-    def k_freq_elem(self,nums:list[int],k:int)->list[int]:
+    def k_freq_elem(self, nums: list[int], k: int) -> list[int]:
         count = {}
         freq = [[] for i in range(len(nums) + 1)]
         for n in nums:
-            count[n] = 1 + count.get(n,0)
-        for n,c in count.items():
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
             freq[c].append(n)
         res = []
 
-        for i  in  range(len(freq) - 1, 0 , -1):
+        for i in range(len(freq) - 1, 0, -1):
             for n in freq[i]:
                 res.append(n)
                 if len(res) == k:
-
                     return res
 
+
 class Solution2:
-    def topKfreqel(self,nums:list[int],k:int)->list[int]:
+    def topKfreqel(self, nums: list[int], k: int) -> list[int]:
         count = {}
         freq = [[] for i in range(len(nums) + 1)]
 
         for n in nums:
-            count[n] = 1 + count.get(n,0)
-        for n,c in count.items():
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
             freq[c].append(n)
         res = []
-        for i  in range(len(freq)- 1,0,-1):
+        for i in range(len(freq) - 1, 0, -1):
             for n in freq[i]:
                 res.append(n)
                 if len(res) == k:
-                    return  res
+                    return res
+
+class Solution3:
+    def topKfreq(self, nums: list[int], k: int) -> list[int]:
+        count = {}  # Hash Map
+        freq = [[] for i in range(len(nums) + 1)]
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+
+
+
+
 
 
 
---
+++ test_freq.py
@@ -0,0 +1,6 @@
+
+
+freq = [[] for  i  in range(12)]
+
+
+print(freq)
