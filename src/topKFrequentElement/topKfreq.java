0d9978978a5aa1747907d04e00579fba0440342e
ryan <ryanwa@ueab.ac.ke>
2023-01-25 09:51:22 +0300

    Product of Array

--- src/topKFrequentElement/topKfreq.java
+++ src/topKFrequentElement/topKfreq.java
@@ -45,60 +45,43 @@ class Solution1 {
     }
 }
 
-class Solution2 {
+class Soluon2 {
     public int[] topKfreq(int[] nums, int k) {
         int[] arr = new int[k];
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
         PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
-        for(Map.Entry<Integer,Integer> it:map.entrySet()){
+        for (Map.Entry<Integer, Integer> it : map.entrySet()) {
             pq.add(it);
-            if(pq.size()>k) pq.poll();
+            if (pq.size() > k) pq.poll();
         }
-        int i  = k;
-        while(!pq.isEmpty()){
-            arr[--i]=pq.poll().getKey();
+        int i = k;
+        while (!pq.isEmpty()) {
+            arr[--i] = pq.poll().getKey();
         }
         return arr;
     }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class TopKfreq {
+    public int[] topKfreq(int[] nums, int k) {
+        int[] res = new int[k];
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int num : nums) hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(
+                (a, b) -> a.getValue() - b.getValue());
+        for(Map.Entry<Integer,Integer> it:hashMap.entrySet()){
+            pq.add(it);
+            if(pq.size()>k) pq.poll();
+        }
+        int i =k;
+        while(!pq.isEmpty()){
+            res[--i] = pq.poll().getKey();
+        }
+        return  res;
+    }
+}
 
 
 
--- src/topKFrequentElement/topKkotln.kt
+++ src/topKFrequentElement/topKkotln.kt
@@ -1,54 +1,96 @@
 package topKFrequentElement
-class topKkotln{
-    fun topKkotln(nums:List<Int>,k:Int): IntArray {
+
+class topKkotln {
+    fun topKkotln(nums: List<Int>, k: Int): IntArray {
         val res = mutableListOf<Int>()
-        val count = HashMap<Int,Int>()
-        val freq = MutableList<MutableList<Int>>(nums.size +1){
+        val count = HashMap<Int, Int>()
+        val freq = MutableList<MutableList<Int>>(nums.size + 1) {
             mutableListOf()
         }
-        for (n in nums){
-            count[n]=count.getOrDefault(n,0) + 1
+        for (n in nums) {
+            count[n] = count.getOrDefault(n, 0) + 1
         }
-        for ((n,c) in count){
+        for ((n, c) in count) {
             freq[c].add(n)
         }
-        for(i in freq.size - 1 downTo 0){
-            for(n in freq[i]){
+        for (i in freq.size - 1 downTo 0) {
+            for (n in freq[i]) {
                 res.add(n)
-                if(res.size ==k){
+                if (res.size == k) {
                     return res.toIntArray()
                 }
             }
         }
-        return  intArrayOf()
+        return intArrayOf()
     }
 }
 
 
-class Solution2{
-    fun topKfreq(nums:IntArray,k: Int):IntArray{
+class Solution2 {
+    fun topKfreq(nums: IntArray, k: Int): IntArray {
         var res = mutableListOf<Int>()
-        var count = HashMap<Int,Int>()
-        var freq = MutableList<MutableList<Int>>(nums.size +1){
+        var count = HashMap<Int, Int>()
+        var freq = MutableList<MutableList<Int>>(nums.size + 1) {
             mutableListOf()
         }
-        for (n in nums){
-            count[n] = count.getOrDefault(n,0) +1
+        for (n in nums) {
+            count[n] = count.getOrDefault(n, 0) + 1
         }
-        for((n,c) in count){
+        for ((n, c) in count) {
             freq[c].add(n)
         }
-        for (i in freq.size - 1 downTo 0){
-            for (n in freq[i]){
+        for (i in freq.size - 1 downTo 0) {
+            for (n in freq[i]) {
                 res.add(n)
-                if(res.size==k){
-                    return  res.toIntArray()
+                if (res.size == k) {
+                    return res.toIntArray()
                 }
             }
         }
-        return  intArrayOf()
+        return intArrayOf()
     }
 }
+
+class Soln {
+    fun topKfreqKt(nums: IntArray, k: Int): IntArray {
+        var arr = mutableListOf<Int>()
+        val count: HashMap<Int, Int> = HashMap<Int, Int>()
+        var freq = MutableList<MutableList<Int>>(nums.size - 1) {
+            mutableListOf()
+        }
+        for (numbers in nums) {
+            count[numbers] = count.getOrDefault(numbers, 0) + 1
+        }
+        for ((n, c) in count) {
+            freq[c].add(n)
+        }
+        for (i in freq.size - 1 downTo 0) {
+            for (n in freq[i]) {
+                arr.add(n)
+                if (arr.size == k) {
+                    return arr.toIntArray()
+                }
+            }
+        }
+        return intArrayOf()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
