dce9533ebd081781ae362112467bddc3f42db991
ryan <ryanwa@ueab.ac.ke>
2023-01-27 15:14:56 +0300

    Product  of ArrayExcept

--- Arrays & Hashing/Product of Array Except Self/prod_array_except.py
+++ Arrays & Hashing/Product of Array Except Self/prod_array_except.py
@@ -56,6 +56,18 @@ class ProdArray:
 
 
 
+class SolnProdArray:
+    def prodArrayExceptSelf(self,nums:list[int])->list[int]:
+        res = [1] * len(nums)
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix=1
+        for i in range(len(nums)-1,-1,-1,):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return  res
 
 
 
