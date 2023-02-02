c0d215d12bb0796a6622cc8c6edf69931ab638e8
ryan <ryanwa@ueab.ac.ke>
2023-02-02 09:33:01 +0300

    Anagram

--- Arrays & Hashing/anagram.py
+++ Arrays & Hashing/anagram.py
@@ -11,3 +11,19 @@ class Soln:
             countS[s[i]] = 1 + countS.get(countS[s[i]], 0)
             countT[t[i]] = 1 + countT.get(countT[t[i]], 0)
         return countT == countS
+
+
+# Given two strings s and  t,  return True if  s is an anagram of t
+# An anagram is a word whose letters can be used to make the second word
+
+class AnagramSol:
+    def anagram(self, s: str, t: str) -> bool:
+        # Check the length of both strings
+        if len(s) != len(t):
+            return False
+        # Create a countS and countT dictionary
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i],0)
+            countT[t[i]] = 1 + countT.get(t[i],0)
+        return  countT == countS
