6673e17600d4274a009656f589b626b8d8c7965c
ryan <ryanwa@ueab.ac.ke>
2023-02-03 10:21:38 +0300

    twoSum review

--- Arrays & Hashing/two_sum.py
+++ Arrays & Hashing/two_sum.py
@@ -23,8 +23,32 @@ class soln:
             return []
 
 
+#Given an integer array  nums and a target k
+#Return indices of two numbers that add up to k
+class SolutionTwoS:
+    def twoSum(self,nums:list[int],k:int)->list[int]:
+        prevMap = {}#val -> index
+        for i,n in enumerate(nums):
+            diff = k - n
+            if diff in prevMap:
+                return  [prevMap[diff], i]
+            prevMap[n] = i
+
+    def twoSum2(self,nums:list[int],k:int):
+        hashMap = {}
+        for key,value in enumerate(nums):
+            diff = k - value
+            if diff in hashMap:
+                return  [hashMap[diff],key]
+            hashMap[value] = key
 
 
-
-
-
+class SolutionTwoSu:
+    def twoSum(self,nums:list[int],target:int):
+        prevMap = {}
+        for index,value in enumerate(nums):
+            #Get th
+            difference = target - value
+            if difference in prevMap:
+                return  [prevMap[difference],value]
+            prevMap[value] = index
