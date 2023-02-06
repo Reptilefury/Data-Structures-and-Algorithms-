6697b5c273c21c771fd5531736a90da04981271f
ryan <ryanwa@ueab.ac.ke>
2023-02-06 10:23:54 +0300

    Group Anagram

--- src/GroupAnagrams/groupAnagrams.kt
+++ src/GroupAnagrams/groupAnagrams.kt
@@ -1,5 +1,4 @@
 package GroupAnagrams
-
 class groupAnagrams {
     fun groupAnagrams(strs: List<String>): List<List<String>> {
         val res: HashMap<String, MutableList<String>> = hashMapOf()
@@ -46,17 +45,24 @@ class Solution3{
                 val index = char - 'a'
                 count[index] += 1
             }
-            res[count.joinToString()] = res.getOrDefault(count.joinToString(), mutableListOf()).also { it.add(s)}
+            res[count.joinToString()] = res.getOrDefault(count.joinToString(), mutableListOf()).also { it.add(string)}
         }
         return  res.values.toList()
 
     }
 }
 
-
-
-
-
-
-
-
+class SolutionGroup{
+    fun groupAnagram(strs:ArrayList<String>): List<MutableList<String>> {
+        val res:HashMap<String,MutableList<String>> = hashMapOf()
+        for(s in strs){
+            val count = IntArray(26)
+            for(c in s){
+                val index = c - 'a'
+                count[index] += 1
+            }
+            res[count.joinToString()] = res.getOrDefault(count.joinToString(), mutableListOf()).also { it.add(s)}
+        }
+        return res.values.toList();
+    }
+}
--- src/TwoSum/TwoSumJava.java
+++ src/TwoSum/TwoSumJava.java
@@ -1,8 +1,12 @@
 package TwoSum;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
-class Solun{
+class Solun {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> hash = new HashMap<>();//value and the index
         for (int i = 0; i < nums.length; i++) {
@@ -19,62 +23,82 @@ class Solun{
 
 //Recall
 class Soln22 {
-    public int[] twoSum(int[] nums,int target){
+    public int[] twoSum(int[] nums, int target) {
         //Create a hashMap to store the index and the value
-        HashMap<Integer,Integer> hash = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
             int num = hash.get(i);
             int difference = target - num;
-            if (hash.containsKey(difference)){
-                return new int[] {hash.get(difference),i};
-            }
-            else{
-                hash.put(num,i);
+            if (hash.containsKey(difference)) {
+                return new int[]{hash.get(difference), i};
+            } else {
+                hash.put(num, i);
             }
         }
         return new int[]{};
     }
 }
-class Soln2Sum{
-    public int[] twoSum(int[] nums, int target){
-        HashMap<Integer,Integer>  hashMap = new HashMap<>();
-        for(int i = 0;i<nums.length;i++){
+
+class Soln2Sum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
             int difference = target - num;
-            if (hashMap.containsKey(difference)){
-                return  new int[] {hashMap.get(difference),i};
+            if (hashMap.containsKey(difference)) {
+                return new int[]{hashMap.get(difference), i};
             }
-            hashMap.put(num,i);
+            hashMap.put(num, i);
         }
-        return new int[] {};
+        return new int[]{};
     }
 }
 
-class  SolnTwS{
-    int[] twoSum(int[] nums,int target){
-        HashMap<Integer,Integer> hashMap = new HashMap<>();
-        for(int i = 0;i<nums.length;i++){
+class SolnTwS {
+    int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
             int difference = target - num;
-            if(hashMap.containsKey(difference)){
-                return  new int[] {hashMap.get(difference),i};
+            if (hashMap.containsKey(difference)) {
+                return new int[]{hashMap.get(difference), i};
             }
-            hashMap.put(num,i);
+            hashMap.put(num, i);
         }
-        return  new int[] {};
+        return new int[]{};
     }
 }
-class prince{
-    public  int[] endJustifies(int[] nums,int target){
-        HashMap<Integer,Integer> hashMap = new HashMap<>();
-        for(int i = 0;i<nums.length;i++){
+
+class prince {
+    public int[] endJustifies(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
             int difference = target - num;
-            if(hashMap.containsKey(difference)){
-                return  new int[] {hashMap.get(difference),i};
+            if (hashMap.containsKey(difference)) {
+                return new int[]{hashMap.get(difference), i};
             }
-            hashMap.put(num,i);
+            hashMap.put(num, i);
         }
-        return  new int[] {};
-        }
+        return new int[]{};
     }
+}
+
+class Solution {
+    public List<List<String>> groupAnagram(String[] strs) {
+        List<List<String>> arr = new ArrayList<>();
+        if (strs.length == 0) return arr;
+        HashMap<String, List<String>> hashMap = new HashMap<>();
+        for (String string : strs) {
+            int[] hash = new int[26];
+            for (char c : string.toCharArray()) {
+                hash[c - 'a']++;
+            }
+            String key = new String(Arrays.toString(hash));
+            hashMap.computeIfAbsent(key, k -> new ArrayList<>());
+            hashMap.get(key).add(string);
+        }
+        arr.addAll(hashMap.values());
+        return arr;
+    }
+}   
