6c9e3f423f57b59d2f9aeedf09bd5c86c3cba6e8
ryan <ryanwa@ueab.ac.ke>
2023-02-06 10:20:28 +0300

    Recap GroupAnagrams

--- Arrays & Hashing/Group Anagrams/group_anagrams.py
+++ Arrays & Hashing/Group Anagrams/group_anagrams.py
@@ -1,6 +1,5 @@
 # Given an array of strings group the anagrams together
 import collections
-
 class Soln:
     def groupAnagram(self, strs: list[str]):
         hash = collections.defaultdict(list)
@@ -26,7 +25,6 @@ class Soln:
 
 
 
-
 #Given an array of strings group the anagrams and return the answer in any order
 class Soln2:
     def group_anagram(self,list_string:list[str])->list[list[str]]:
@@ -38,11 +36,28 @@ class Soln2:
             hash_map[tuple(count)].append(string)
         return  hash_map.values()
 
+#Given an array of strings strs, group anagrams together
+# and return the answer in any  order
+class Solution:
+    def groupAnagrams(self,strs:list[str])->list[list[str]]:
+        ans = collections.defaultdict(list)
+        for string  in  strs:
+            count = [0] * 26
+            for char in string:
+                count[ord(char) - ord('a')] +=1
+            ans[tuple(count)].append(string)
+        return ans.values()
 
 
-
-
-
+class Solution2:
+    def groupAnagrams(self,strs:list[str])->list[list[str]]:
+        ans = collections.defaultdict(list)
+        for string in  strs:
+            count = [0] * 26
+            for char in string:
+                count[ord(string) - ord('a')]+=1
+            ans[tuple(count)].append(string)
+        return ans.values()
 
 
 
