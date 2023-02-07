b942cc31910c8f945dc9a7a8e0a05a1109a4a4d1
ryan <ryanwa@ueab.ac.ke>
2023-02-07 09:48:33 +0300

    Group Anagram:DSA recap

--- Arrays & Hashing/Group Anagrams/group_anagrams.py
+++ Arrays & Hashing/Group Anagrams/group_anagrams.py
@@ -1,5 +1,7 @@
 # Given an array of strings group the anagrams together
 import collections
+
+
 class Soln:
     def groupAnagram(self, strs: list[str]):
         hash = collections.defaultdict(list)
@@ -24,46 +26,50 @@ class Soln:
         return hash_map.values()
 
 
-
-#Given an array of strings group the anagrams and return the answer in any order
+# Given an array of strings group the anagrams and return the answer in any order
 class Soln2:
-    def group_anagram(self,list_string:list[str])->list[list[str]]:
+    def group_anagram(self, list_string: list[str]) -> list[list[str]]:
         hash_map = collections.defaultdict(list)
         for string in list_string:
             count = [0] * 26
             for char in string:
                 count[ord(char) - ord('a')] += 1
             hash_map[tuple(count)].append(string)
-        return  hash_map.values()
+        return hash_map.values()
 
-#Given an array of strings strs, group anagrams together
+
+# Given an array of strings strs, group anagrams together
 # and return the answer in any  order
 class Solution:
-    def groupAnagrams(self,strs:list[str])->list[list[str]]:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         ans = collections.defaultdict(list)
-        for string  in  strs:
+        for string in strs:
             count = [0] * 26
             for char in string:
-                count[ord(char) - ord('a')] +=1
+                count[ord(char) - ord('a')] += 1
             ans[tuple(count)].append(string)
         return ans.values()
 
 
 class Solution2:
-    def groupAnagrams(self,strs:list[str])->list[list[str]]:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         ans = collections.defaultdict(list)
-        for string in  strs:
+        for string in strs:
             count = [0] * 26
             for char in string:
-                count[ord(string) - ord('a')]+=1
+                count[ord(string) - ord('a')] += 1
             ans[tuple(count)].append(string)
         return ans.values()
 
 
 
-
-
-
-
-
-
+# Given an array of strings group anagrams together and return in any order
+class SolnGroupAnagram:
+    def groupAnagram(self, strs: list[str]) -> list[list[str]]:
+        ans = collections.defaultdict(list)
+        for string in strs:
+            count = [0] * 26
+            for char in string:
+                count[ord(char) - ord('a')] += 1
+            ans[tuple(count)].append(string)
+        return ans.values()
