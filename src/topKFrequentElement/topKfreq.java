a79a3a4b4cf94bc1031d3c399e2478c951a165dd
ryan <ryanwa@ueab.ac.ke>
2023-02-08 09:57:09 +0300

    topKfreq elements

--- src/topKFrequentElement/topKfreq.java
+++ src/topKFrequentElement/topKfreq.java
@@ -80,16 +80,41 @@ class TopKfreq {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+class TopKfreqElement{
+    public int[] topKfreq(int[] nums,int k){
+        int[] arr = new int[k];
+        HashMap<Integer,Integer> hashMap = new  HashMap<>();
+        for(int num:nums) hashMap.put(num, hashMap.getOrDefault(num,0)+1);
+        PriorityQueue<Map.Entry<Integer,Integer>> pq = new  PriorityQueue<>(
+                (a,b) -> a.getValue() - b.getValue()
+        );
+        for (Map.Entry<Integer,Integer> it:hashMap.entrySet()){
+            pq.add(it);
+            if(pq.size()>k) pq.poll();
+        }
+        int i = k;
+        while(!pq.isEmpty()){
+            arr[--i] = pq.poll().getKey();
+        }
+        return  arr;
+    }
+}
+class TopKfreqElementQ{
+    public  int[] topKfreq(int[] nums, int k){
+        int[] arr = new int[k];
+        HashMap<Integer,Integer>  map = new  HashMap<>();
+        for(int num:nums) map.put(num, map.getOrDefault(num,0)+1);
+        PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>(
+                (a,b) -> a.getValue() -b.getValue()
+        );
+        for (Map.Entry<Integer,Integer> it:map.entrySet()){
+            pq.add(it);
+            if(pq.size() > k) pq.poll();
+        }
+        int i = k;
+        while(!pq.isEmpty()){
+            arr[--i] = pq.poll().getKey();
+        }
+        return  arr;
+    }
+}
