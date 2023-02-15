c09f8caeab8c3c519cb57c0d956a14972f334b76
Ryan Wamukota <rwamukota@hq.ekenya.co.ke>
2023-02-15 08:39:28 +0300

    Valid Sudoku

--- src/validSudoku/ValidSudoku.java
+++ src/validSudoku/ValidSudoku.java
@@ -1,6 +1,21 @@
 package validSudoku;
 
-public class ValidSudoku{
-
-    
+public class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        int rows = board.length;
+        int cols = board[0].length;
+        Set<Character> rowSet = null;
+        Set<Character> colSet = null;
+        for (int i = 0; i < rows; i++) {
+            rowSet = new HashSet<>();
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                if (rowSet.contains(board[i][j])) {
+                    return  false;
+                }
+            }
+        }
+    }
 }
