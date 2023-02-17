908926f0fd29009ad4bc16f844e6c3629aaf6745
Ryan Wamukota <rwamukota@hq.ekenya.co.ke>
2023-02-17 08:48:13 +0300

    valid_sudoku

--- valid sudoku/valid_sudoku.py
+++ valid sudoku/valid_sudoku.py
@@ -51,3 +51,21 @@ class SolutionIsValidSudoku:
                 cols[c].add(board[r][c])
                 squares[(r // 3, c // 3)].add(board[r][c])
         return True
+
+
+class ValidSudoku:
+    def is_valid_board(self, board: list[list[str]]) -> bool:
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if (board[r][c] in cols or board[r][c] in rows or board[r][c] in squares[(r // 3, c // 3)]):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+        return True
