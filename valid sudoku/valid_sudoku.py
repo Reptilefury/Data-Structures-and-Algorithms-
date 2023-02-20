44497b214a66c566386a302c725fed135ba20ee8
Ryan Wamukota <rwamukota@hq.ekenya.co.ke>
2023-02-20 09:01:37 +0300

    Valid Sudoku

--- valid sudoku/valid_sudoku.py
+++ valid sudoku/valid_sudoku.py
@@ -69,3 +69,38 @@ class ValidSudoku:
                 cols[c].add(board[r][c])
                 squares[(r // 3, c // 3)].add(board[r][c])
         return True
+
+
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]):
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in cols[c] or board[r][c] in rows[r] or board[r][c] in squares[(r // 3, c // 3)]):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+        return True
+
+
+class IsValidSudoku:
+    def isValid(self, board: list[list[str]]) -> bool:
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] in ".":
+                    continue
+                if (board[r][c] in cols[c] or board[r][c] in rows[r] or board[r][c] in squares[(r // 3, c // 3)]):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+        return True
