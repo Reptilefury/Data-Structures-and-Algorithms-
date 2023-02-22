f0ec397216aa2693fd4b96dedd9feba558ff976c
Ryan Wamukota <rwamukota@hq.ekenya.co.ke>
2023-02-22 08:21:00 +0300

    Encode and Decode

--- Encode and Decode Strings/encode_decode_strings.py
+++ Encode and Decode Strings/encode_decode_strings.py
@@ -27,13 +27,49 @@ class SolutionDecEnc:
             return s
 
     def decode(self, s):
-        res,i = [],0
+        res, i = [], 0
 
         while i < len(s):
             j = i
 
         while s[j] != "#":
-            j   += 1
+            j += 1
         length = int(s[i:j])
-        res.append(s[i + 1 : j + 1 + length ])
+        res.append(s[i + 1: j + 1 + length])
+        return res
+
+
+class SolutionEncodeDec:
+    def encode(self, strs):
+        res = ""
+        for s in strs:
+            res += str(len(strs)) + "#" + s
+        return res
+
+    def decode(self, strs):
+        res, i = [], 0
+        while i < len(strs):
+            j = i
+        while strs[j] != "#":
+            j += 1
+        length = int(strs[i:j])
+        res.append(strs[i + j: j + 1 + length])
+        return res
+
+class Solution2:
+    def encode(self,strs):
+        res = ""
+        for s in strs:
+            res += str(len(strs)) + "#" + s
         return  res
+
+    def decode(self,strs):
+        res,i = [],0
+        while i < len(strs):
+            j = i
+        while  strs[j] != "#":
+            j += 1
+        length = int(strs[i:j])
+        res.append(strs[i +1 : j+1  + length])
+        return res
+
