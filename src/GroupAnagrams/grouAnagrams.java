8b0a8c8c65810e32a3b794be28b4b18e0848eadd
ryan <wamukota.ryan@eclectics.io>
2023-03-10 08:43:40 +0300

    group Anagrams

--- src/GroupAnagrams/grouAnagrams.java
+++ src/GroupAnagrams/grouAnagrams.java
@@ -1,8 +1,7 @@
 package GroupAnagrams;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import javax.swing.plaf.ListUI;
+import java.util.*;
+
 public class grouAnagrams {
     public List<List<String>> grpAnagrams(String[] str) {
         List<List<String>> res = new ArrayList<>();
@@ -110,7 +109,40 @@ class GroupAnagrams{
     }
 }
 
+class SolutionGroup1{
+    public List<List<String>> solGroup(String[] strs){
+        List<List<String>> res = new ArrayList<>();
+        if(strs.length == 0) return res;
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String s:strs){
+            int[] hash = new int[26];
+            for(char c:s.toCharArray()){
+                hash[c - 'a']++;
+            }
+            String key = new String(Arrays.toString(hash));
+            map.computeIfAbsent(key,k-> new ArrayList<>());
+            map.get(key).add(s);
+        }
+        res.addAll(map.values());
+        return  res;
+    }
+}
 
-
-
-
+class  SolnGrp{
+    public  List<List<String>> groupAnagram(String[] strs){
+        List<List<String>> res = new ArrayList<>();
+        if(strs.length == 0) return  res;
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String s:strs){
+            int[] hash =  new int[26];
+            for(char c:s.toCharArray()){
+                hash[c - 'a']++;
+            }
+            String key = new String(Arrays.toString(hash));
+            map.computeIfAbsent(key,k-> new ArrayList<>());
+            map.get(key).add(s);
+        }
+        res.addAll(map.values());
+        return res;
+    }
+}
