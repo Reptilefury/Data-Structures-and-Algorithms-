36e39953aed52b91cd3485636400e7b31a802ccf
ryan <wamukota.ryan@eclectics.io>
2023-03-14 10:03:26 +0300

    productArray

--- Arrays & Hashing/Product of Array Except Self/prod_array_except.py
+++ Arrays & Hashing/Product of Array Except Self/prod_array_except.py
@@ -73,18 +73,49 @@ class ProdArrayExcept:
 
 
 
+#Given an integer array nums, return an array answer such that
+#answer[i] is equal to the product of all elements except self
+class Solution22:
+    def prodArrayExceptSelf(self,nums:list[int])->list[int]:
+        res = [1] * (len(nums))
+        prefix =1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+
+class Solution222:
+    def productArrayExceptSelf(self,nums:list[int],):
+        res = [1] * (len(nums))
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) -1, -1,-1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
 
 
 
 
-
-
-
-
-
-
-
-
+class lastSoln:
+    def prodArrayExcptSelf(self,nums:list[int])-> list[int]:
+        res = [1] * (len(nums))
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
 
 
 
