7955740cef6cc25fcd203af37432dc49f38bc8e0
ryan <wamukota.ryan@eclectics.io>
2023-03-14 10:02:43 +0300

    product Array Except Self

--- src/productOfArray/prodArrayExceptSelf.java
+++ src/productOfArray/prodArrayExceptSelf.java
@@ -1,6 +1,5 @@
 package productOfArray;
-
-public class ProdArrayExceptSelf{
+public class prodArrayExceptSelf {
     public  int[] prodExceptSelf(int[] nums){
         int[] arr = new int[nums.length];
         int right = 1; int left = 1;
@@ -47,3 +46,52 @@ class ProductArrayExceptSelf{
         return arr;
     }
 }
+
+class Soln{
+    public  int[] productExceptSelf(int[] nums){
+        int[] array = new int[nums.length];
+        int right =1, left =1;
+        for(int i = 0; i < nums.length; i++){
+            array[i] = left;
+            left *= nums[i];
+        }
+        for(int i = nums.length -1; i >=0;i--){
+            array[i] *= right;
+            right *= nums[i];
+        }
+        return array;
+     }
+     public int[] productArrayExceptSelfNumsPrefix(int[] nums){
+        int[] output = new int[nums.length];
+        output[0] = 1;
+
+        for(int i =0;i<nums.length -1;i++) output[i + 1] = output[i] * nums[i];
+        for(int i = nums.length -2; i >= 0;i++){
+            output[i] = nums[i + 1] * output[i];
+            nums[i] = nums[i] * nums[i + 1];
+        }
+        return output;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
