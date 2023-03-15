72a937fb840b76d8ae7b0b4990a87589392427f5
ryan <wamukota.ryan@eclectics.io>
2023-03-15 08:49:20 +0300

    is Valid Sudoku

--- Arrays & Hashing/valid sudoku/valid_sudoku.py
+++ Arrays & Hashing/valid sudoku/valid_sudoku.py
@@ -1,6 +1,5 @@
 import collections
 
-
 class Solution:
     def isValidSudoku(self, board: list[list[str]]) -> bool:
         rows = collections.defaultdict(set)
@@ -103,4 +102,21 @@ class IsValidSudoku:
                 rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
                 squares[(r // 3, c // 3)].add(board[r][c])
+        return True
+class Solution:
+    def isValidSudoku(self,board:list[list[str]])->bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r//3,c//3)]
+                ):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3,c//3)].add(board[r][c])
         return True
