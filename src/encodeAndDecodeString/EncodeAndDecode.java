a1b98fc588a971a5671aa4aced6134de68e589fc
ryan <wamukota.ryan@eclectics.io>
2023-03-16 09:25:00 +0300

    Encode and Decode Strings

--- src/encodeAndDecodeString/EncodeAndDecode.java
+++ src/encodeAndDecodeString/EncodeAndDecode.java
@@ -73,9 +73,29 @@ class EncodeDecodeSol {
     }
 }
 
-
-
-
+class EncodeAndDecode1{
+    public String encodeAndDecodeString(List<String> strs){
+        StringBuilder encodedString = new StringBuilder();
+        for(String str:strs){
+            encodedString.append(str.length()).append("#").append(str);
+        }
+        return  encodedString.toString();
+    }
+    public  List<String> decodeString(String str){
+        List<String> list = new ArrayList<>();
+        int  i = 0;
+        while(i<str.length()){
+            int  j = i;
+            while(str.charAt(j) != '#'){
+                j++;
+            }
+            int length = Integer.valueOf(str.substring(i,j));
+            i = j + 1 + length;
+            list.add(str.substring(j + 1,i));
+        }
+        return list;
+    }
+}
 
 
 
