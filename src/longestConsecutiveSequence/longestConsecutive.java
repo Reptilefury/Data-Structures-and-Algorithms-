16e63463fe682388f37ffed4b2cd26f48894e9e5
ryan <wamukota.ryan@eclectics.io>
2023-03-17 08:47:50 +0300

    Longest Consecutive

--- src/longestConsecutiveSequence/longestConsecutive.java
+++ src/longestConsecutiveSequence/longestConsecutive.java
@@ -164,3 +164,71 @@ class LongestConsecutiveSol {
         return ans;
     }
 }
+class Soln{
+    public  int longestConsecutive(int[] nums){
+        if (nums.length == 0) return  0;
+        HashSet<Integer> set = new HashSet<>();
+        int ans =1;
+        for(int num:nums) set.add(num);
+        for(int num:nums){
+            if(!set.contains(num - 1)){
+                int count =1;
+                while (set.contains(num + 1)){
+                    num++;
+                    count++;
+                }
+                ans = Math.max(count,ans);
+            }
+        }
+        return ans;
+    }
+}
+
+class SolN{
+    public int longestConsecutive(int[] nums){
+        if (nums.length == 0) return  0;
+        HashSet<Integer> set = new HashSet<>();
+        int ans = 1;
+        for(int num:nums) set.add(num);
+        for(int num:nums){
+            if(!set.contains(num -1)){
+                int count = 1;
+                while(set.contains(num +1)){
+                    num++;
+                    count++;
+                }
+                ans = Math.max(num,count);
+            }
+        }
+        return ans;
+    }
+}
+
+class LongestConsecutiveLast{
+    public  int longestConsecutive(int[] nums){
+        if(nums.length == 0) return  0;
+        HashSet<Integer> set = new HashSet<>();
+        int ans =1;
+        for(int num:nums) set.add(num);
+        for(int num:nums){
+            if(!set.contains(num)){
+                int count = 1;
+                while(set.contains(num + 1)){
+                    num++;
+                    count++;
+                }
+                ans = Math.max(ans,count);
+            }
+        }
+        return ans;
+    }
+}
+
+
+
+
+
+
+
+
+
