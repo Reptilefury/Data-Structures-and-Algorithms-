d0ae7f11e542c0b0f9709d7948e86952a7c0c1a0
ryan <wamukota.ryan@eclectics.io>
2023-09-20 04:05:01 +0300

    three sum

--- TwoPointers/two_sum.py
+++ TwoPointers/two_sum.py
@@ -1,7 +1,7 @@
 # Given a list of numbers with a target number, return the indices of two numbers that add up to the target number
 class TwoSum:
     def isTwoSum(self, numbers: list[int], target: int):
-        left, right = 0, len(numbers) -1
+        left, right = 0, len(numbers) - 1
         while (left < right):
             currentSum = numbers[left] + numbers[right]
             if (currentSum > target):
@@ -10,3 +10,15 @@ class TwoSum:
                 left += 1
             else:
                 return [left + 1, right + 1]
+
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        left, right = 0, len(1) - 1
+        while left < right:
+            currentSum = numbers[left] = numbers[right]
+            if currentSum > target:
+                right -= 1
+            elif currentSum < target:
+                left += target
+            else:
+                return [1 + left, 1 + right]
+        return []
